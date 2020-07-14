a72171941a3d31301ca106e386a778b342a44d76
Asher Hamrick <ashhamrick2@gmail.com>
2020-07-13 20:10:11 -0400

    Basic Server Config

---
+++ server/server.js
@@ -0,0 +1,12 @@
+// Server initialization
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
