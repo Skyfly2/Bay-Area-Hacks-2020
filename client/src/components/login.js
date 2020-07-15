c77f067165e57342d36b81d69f130347c6a3b88e
Asher Hamrick <ashhamrick2@gmail.com>
2020-07-14 22:37:05 -0400

    Prepare frontend components for integration with backend

--- client/src/components/login.js
+++ client/src/components/login.js
@@ -1,17 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function Login(props) {
-    return (
-        <div className="content">
-            <div className="center login">
-                <h1>Regimen</h1>
-                <input className="input" type="text" placeholder="Username"></input>
-                <input className="input" type="password" placeholder="Password"></input>
-                <button className="btn-sub">Login</button>
-                <button className="btn-logout" onClick={props.register}>Don't have an account? Create one here.</button>
-            </div>
-        </div>
-    );
+    // Declare States
+    const [user, changeUser] = useState(null);
+    const [pass, changePass] = useState(null);
+    const [status, changeStatus] = useState(null);
+
+    switch (status) {
+        default:
+            return (
+                <div className="content">
+                    <div className="center login">
+                        <h1>Regimen</h1>
+                        <input className="input" type="text" placeholder="Username" onChange={e => changeUser(e.target.value)} value={user}></input>
+                        <input className="input" type="password" placeholder="Password" onChange={e => changePass(e.target.value)} value={pass}></input>
+                        <button className="btn-sub">Login</button>
+                        <button className="btn-logout" onClick={props.register}>Don't have an account? Create one here.</button>
+                    </div>
+                </div>
+            );
+    }
 }
 
 export default Login;
--- client/src/components/register.js
+++ client/src/components/register.js
@@ -1,19 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function Register(props) {
-    return (
-        <div className="content">
-            <div className="center login">
-                <h1>Register</h1>
-                <input className="input" type="text" placeholder="Firstname"></input>
-                <input className="input" type="text" placeholder="Lastname"></input>
-                <input className="input" type="text" placeholder="Username"></input>
-                <input className="input" type="password" placeholder="Password"></input>
-                <button className="btn-sub">Register</button>
-                <button className="btn-logout" onClick={props.login}>Already have an account? Login here.</button>
-            </div>
-        </div>
-    );
+    // Declare States
+    const [user, updateUser] = useState(null);
+    const [pass, updatePass] = useState(null);
+    const [cpass, updateCPass] = useState(null);
+    const [first, updateFirst] = useState(null);
+    const [last, updateLast] = useState(null);
+    const [status, changeStatus] = useState(null);
+
+    switch (status) {
+        default:
+            return (
+                <div className="content">
+                    <div className="center login">
+                        <h1>Register</h1>
+                        <input className="input" type="text" placeholder="Firstname" onChange={e => updateFirst(e.target.value)} value={first}></input>
+                        <input className="input" type="text" placeholder="Lastname" onChange={e => updateLast(e.target.value)} value={last}></input>
+                        <input className="input" type="text" placeholder="Username" onChange={e => updateUser(e.target.value)} value={user}></input>
+                        <input className="input" type="password" placeholder="Password" onChange={e => updatePass(e.target.value)} value={pass}></input>
+                        <input className="input" type="password" placeholder="Password" onChange={e => updateCPass(e.target.value)} value={cpass}></input>
+                        <button className="btn-sub">Register</button>
+                        <button className="btn-logout" onClick={props.login}>Already have an account? Login here.</button>
+                    </div>
+                </div>
+            );
+    }
+
+
 }
 
 export default Register;
