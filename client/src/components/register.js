aa2c62fab99bab08452d263987ea85e45d58a586
Asher Hamrick <ashhamrick2@gmail.com>
2020-07-14 23:11:27 -0400

    Bad input handling

--- client/src/components/register.js
+++ client/src/components/register.js
@@ -4,13 +4,22 @@ import axios from 'axios';
 function Register(props) {
     const register = async e => {
         e.preventDefault();
+        if (user === '' || pass === '' || first === '' || last === '') {
+            return changeStatus('blank');
+        }
+        else if (pass !== cpass) {
+            return changeStatus('pass-no-match');
+        }
         let response = await axios.post('http://localhost:4000/register', {
             user: user,
             pass: pass,
             first: first,
             last: last
         });
-        console.log(response);
+        if (response.data.status === 'taken') {
+            return changeStatus('taken');
+        }
+        return props.login;
     }
 
     // Declare States
@@ -22,6 +31,54 @@ function Register(props) {
     const [status, changeStatus] = useState(null);
 
     switch (status) {
+        case 'pass-no-match':
+            return (
+                <div className="content">
+                    <div className="center login">
+                        <h1>Register</h1>
+                        <p className="red">Passwords do not match</p>
+                        <input className="input" type="text" placeholder="Firstname" onChange={e => updateFirst(e.target.value)} value={first}></input>
+                        <input className="input" type="text" placeholder="Lastname" onChange={e => updateLast(e.target.value)} value={last}></input>
+                        <input className="input" type="text" placeholder="Username" onChange={e => updateUser(e.target.value)} value={user}></input>
+                        <input className="input-err" type="password" placeholder="Password" onChange={e => updatePass(e.target.value)} value={pass}></input>
+                        <input className="input-err" type="password" placeholder="Confirm Password" onChange={e => updateCPass(e.target.value)} value={cpass}></input>
+                        <button className="btn-sub" onClick={register}>Register</button>
+                        <button className="btn-logout" onClick={props.login}>Already have an account? Login here.</button>
+                    </div>
+                </div>
+            );
+        case 'taken':
+            return (
+                <div className="content">
+                    <div className="center login">
+                        <h1>Register</h1>
+                        <p className="red">Username is already taken</p>
+                        <input className="input" type="text" placeholder="Firstname" onChange={e => updateFirst(e.target.value)} value={first}></input>
+                        <input className="input" type="text" placeholder="Lastname" onChange={e => updateLast(e.target.value)} value={last}></input>
+                        <input className="input-err" type="text" placeholder="Username" onChange={e => updateUser(e.target.value)} value={user}></input>
+                        <input className="input" type="password" placeholder="Password" onChange={e => updatePass(e.target.value)} value={pass}></input>
+                        <input className="input" type="password" placeholder="Confirm Password" onChange={e => updateCPass(e.target.value)} value={cpass}></input>
+                        <button className="btn-sub" onClick={register}>Register</button>
+                        <button className="btn-logout" onClick={props.login}>Already have an account? Login here.</button>
+                    </div>
+                </div>
+            );
+        case 'blank':
+            return (
+                <div className="content">
+                    <div className="center login">
+                        <h1>Register</h1>
+                        <p className="red">Please fill out all highlighted fields</p>
+                        <input className="input-err" type="text" placeholder="Firstname" onChange={e => updateFirst(e.target.value)} value={first}></input>
+                        <input className="input-err" type="text" placeholder="Lastname" onChange={e => updateLast(e.target.value)} value={last}></input>
+                        <input className="input-err" type="text" placeholder="Username" onChange={e => updateUser(e.target.value)} value={user}></input>
+                        <input className="input-err" type="password" placeholder="Password" onChange={e => updatePass(e.target.value)} value={pass}></input>
+                        <input className="input-err" type="password" placeholder="Confirm Password" onChange={e => updateCPass(e.target.value)} value={cpass}></input>
+                        <button className="btn-sub" onClick={register}>Register</button>
+                        <button className="btn-logout" onClick={props.login}>Already have an account? Login here.</button>
+                    </div>
+                </div>
+            );
         default:
             return (
                 <div className="content">
